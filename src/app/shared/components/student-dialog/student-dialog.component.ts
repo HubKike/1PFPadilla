c1981a84c2c9972677a369c030b99d436d1f7083
Enrique Padilla <ultrared808@gmail.com>
2022-12-21 00:02:37 -0600

    1PFPadilla_Inicio

---
+++ src/app/shared/components/student-dialog/student-dialog.component.ts
@@ -0,0 +1,32 @@
+import { DialogRef } from '@angular/cdk/dialog';
+import { Component, Inject } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Student } from 'src/app/models/student.model';
+
+@Component({
+  selector: 'app-student-dialog',
+  templateUrl: './student-dialog.component.html',
+  styles: [
+  ]
+})
+export class StudentDialogComponent {
+  firstNameControl = new FormControl('');
+  lastNameControl = new FormControl('');
+  studentForm = new FormGroup({
+    firstName: this.firstNameControl,
+    lastName: this.lastNameControl
+  })
+
+  constructor(private readonly dialogRef: DialogRef, @Inject(MAT_DIALOG_DATA) public data: Student | null) {
+    if (data) {
+      console.log(data)
+      this.studentForm.patchValue(data);
+    }
+  }
+
+  close() {
+    this.dialogRef.close()
+  }
+
+}
